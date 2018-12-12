f203f3f605499fb5d33337d6c7f9dacefad14246
Andrey <luzkovskyandrey@gmail.com>
2018-12-13 00:40:17 +0300

    eslint, start refactoring components

--- src/containers/Settings/Settings.jsx
+++ src/containers/Settings/Settings.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../redux/settings';
 
-import Container from '../../components/Container/Container';
 import Titile from '../../components/Titile/Titile';
 import Button from '../../components/Button/Button';
 import Block, { BlockRow } from '../../components/Settings/Block';
@@ -14,7 +13,7 @@ import {
     SettingLabel, SettingRow,
     SettingsContainer,
 } from '../../components/Settings/Settings';
-import RootRegistry from '../../components/RootRegistry/RootRegistry';
+import ScrollContainer from '../../components/ScrollContainer/ScrollContainer';
 
 
 class Settings extends Component {
@@ -74,89 +73,87 @@ class Settings extends Component {
         } = this.props;
 
         return (
-            <RootRegistry>
-                <Container>
-                    <Titile>/ Settings</Titile>
-                    <SettingsContainer>
-                        <ConnectionContainer>
-                            <Titile inline>CONNECTION</Titile>
-                            <Block>
+            <ScrollContainer>
+                <Titile>/ Settings</Titile>
+                <SettingsContainer>
+                    <ConnectionContainer>
+                        <Titile inline>CONNECTION</Titile>
+                        <Block>
 
-                                <BlockRow>
-                                    <SettingRow>
-                                        <SettingLabel>IPFS node:</SettingLabel>
-                                        <Input
-                                          inputRef={node => this.ipfsInput = node }
-                                          defaultValue={ IPFS_END_POINT }
-                                          style={ { width: 200 } }
-                                        />
-                                        <Button onClick={ this.updateIPFS }>update</Button>
-                                    </SettingRow>
-                                </BlockRow>
+                            <BlockRow>
+                                <SettingRow>
+                                    <SettingLabel>IPFS node:</SettingLabel>
+                                    <Input
+                                      inputRef={node => this.ipfsInput = node }
+                                      defaultValue={ IPFS_END_POINT }
+                                      style={ { width: 200 } }
+                                    />
+                                    <Button onClick={ this.updateIPFS }>update</Button>
+                                </SettingRow>
+                            </BlockRow>
 
-                                <BlockRow>
-                                    <SettingRow>
-                                        <SettingLabel>ETH node:</SettingLabel>
-                                        <Input
-                                          inputRef={node => this.ethInput = node }
-                                          defaultValue={ PARITY_END_POINT }
-                                          style={ { width: 200 } }
-                                        />
-                                        <Button onClick={ this.setEthCustom }>update</Button>
-                                    </SettingRow>
-                                    <SettingRow style={{marginTop: 20}}>
-                                        <Button onClick={this.setEthMain}>Main</Button>
-                                        <Button onClick={this.setEthRinkeby}>Rikenby</Button>
-                                        <Button onClick={this.setEthKovan}>Kovan</Button>
-                                    </SettingRow>
-                                </BlockRow>
+                            <BlockRow>
+                                <SettingRow>
+                                    <SettingLabel>ETH node:</SettingLabel>
+                                    <Input
+                                      inputRef={node => this.ethInput = node }
+                                      defaultValue={ PARITY_END_POINT }
+                                      style={ { width: 200 } }
+                                    />
+                                    <Button onClick={ this.setEthCustom }>update</Button>
+                                </SettingRow>
+                                <SettingRow style={{marginTop: 20}}>
+                                    <Button onClick={this.setEthMain}>Main</Button>
+                                    <Button onClick={this.setEthRinkeby}>Rikenby</Button>
+                                    <Button onClick={this.setEthKovan}>Kovan</Button>
+                                </SettingRow>
+                            </BlockRow>
 
-                                <BlockRow>
-                                    <SettingRow>
-                                        <SettingLabel>cyberd node:</SettingLabel>
-                                        <Input
-                                          inputRef={node => this.cyberdInput = node }
-                                          defaultValue={ CYBERD_END_POINT }
-                                          style={ { width: 200 } }
-                                        />
-                                        <Button onClick={ this.updateCyberd }>update</Button>
-                                    </SettingRow>
-                                </BlockRow>
+                            <BlockRow>
+                                <SettingRow>
+                                    <SettingLabel>cyberd node:</SettingLabel>
+                                    <Input
+                                      inputRef={node => this.cyberdInput = node }
+                                      defaultValue={ CYBERD_END_POINT }
+                                      style={ { width: 200 } }
+                                    />
+                                    <Button onClick={ this.updateCyberd }>update</Button>
+                                </SettingRow>
+                            </BlockRow>
 
-                            </Block>
-                        </ConnectionContainer>
-                        <NodeStatusContainer>
-                            <Titile inline>STATUS</Titile>
-                            <Block>
-                                <BlockRow>
-                                    <SettingRow>
-                                        <SettingsIndicator status={ ipfsStatus } />
-                                    </SettingRow>
-                                </BlockRow>
-                                <BlockRow style={ { height: 110 } }>
-                                    <SettingRow>
-                                        <SettingsIndicator status={ parityStatus } />
-                                    </SettingRow>
-                                </BlockRow>
-                                <BlockRow>
-                                    <SettingRow>
-                                        <SettingsIndicator status={ cyberdStatus } />
-                                    </SettingRow>
-                                </BlockRow>
-                            </Block>
-                        </NodeStatusContainer>
-                    </SettingsContainer>
+                        </Block>
+                    </ConnectionContainer>
+                    <NodeStatusContainer>
+                        <Titile inline>STATUS</Titile>
+                        <Block>
+                            <BlockRow>
+                                <SettingRow>
+                                    <SettingsIndicator status={ ipfsStatus } />
+                                </SettingRow>
+                            </BlockRow>
+                            <BlockRow style={ { height: 110 } }>
+                                <SettingRow>
+                                    <SettingsIndicator status={ parityStatus } />
+                                </SettingRow>
+                            </BlockRow>
+                            <BlockRow>
+                                <SettingRow>
+                                    <SettingsIndicator status={ cyberdStatus } />
+                                </SettingRow>
+                            </BlockRow>
+                        </Block>
+                    </NodeStatusContainer>
+                </SettingsContainer>
 
-                    <Block>
-                        <BlockRow>
-                            <SettingRow>
-                                <Button color='green' dura='rr.cyb'>CYB ROOT REGISTRY</Button>
-                                <Button onClick={ resetAllSettings }>RESET SETTINGS</Button>
-                            </SettingRow>
-                        </BlockRow>
-                    </Block>
-                </Container>
-            </RootRegistry>
+                <Block>
+                    <BlockRow>
+                        <SettingRow>
+                            <Button color='green' dura='rr.cyb'>CYB ROOT REGISTRY</Button>
+                            <Button onClick={ resetAllSettings }>RESET SETTINGS</Button>
+                        </SettingRow>
+                    </BlockRow>
+                </Block>
+            </ScrollContainer>
         );
     }
 }
